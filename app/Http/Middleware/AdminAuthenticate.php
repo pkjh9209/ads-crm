c8f9af2ae780ef7a321f7d460f2d97a459ec8c2b
pkjh9209 <pkjh20177@gmail.com>
2023-02-06 14:53:06 +0900

    marge

---
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+// use Illuminate\Http\Request;
+use Illuminate\Auth\Middleware\Authenticate as Middleware;
+
+class AdminAuthenticate extends Middleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    // public function handle(Request $request, Closure $next)
+    // {
+    //     return $next($request);
+    // }
+
+    protected function redirectTo($request)
+    {
+        if (!$request->expectsJson()) {
+            return route('admin.login');
+        }
+    }
+}
---
+++ database/migrations/2023_02_06_023910_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('adm_id')->unique();
+            $table->string('adm_name');
+            $table->string('adm_email');
+            $table->string('adm_password');
+            $table->integer('adm_level');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
